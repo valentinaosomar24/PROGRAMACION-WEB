fdf12920e34e5771f561e880f9663066c312b982
valentinaosomar24 <kosorioma@uninpahu.edu.co>
2023-09-24 18:53:39 -0500

    Se crea encabezado de la pagina principal de vilvaos

---
+++ Vilvaos/Vilvaos/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vilvaos.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Producto() 
+        {
+            return View();
+        }
+        
+        public ActionResult Cliente() 
+        {
+            return View();
+        }
+        
+        public ActionResult Proveedor() 
+        {
+            return View();
+        }
+    }
+}
