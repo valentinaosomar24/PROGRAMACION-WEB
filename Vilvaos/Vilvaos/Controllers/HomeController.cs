59cd99347fb9b96834c5cffc0e88cc57f143354f
20KevinA03 <kevintabuo@gmail.com>
2023-11-09 11:07:23 -0500

    Se complementa gran apartado del Front

--- Vilvaos/Vilvaos/Controllers/HomeController.cs
+++ Vilvaos/Vilvaos/Controllers/HomeController.cs
@@ -17,13 +17,28 @@ namespace Vilvaos.Controllers
         {
             return View();
         }
-        
+
+        public ActionResult AgregarProducto()
+        {
+            return View();
+        }
+
         public ActionResult Cliente() 
         {
             return View();
         }
-        
+
+        public ActionResult AgregarCliente()
+        {
+            return View();
+        }
+
         public ActionResult Proveedor() 
+        {
+            return View();
+        }
+
+        public ActionResult AgregarProveedor() 
         {
             return View();
         }
